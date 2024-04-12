a785f880a57fd2e8ce917c747a3771084df9943d
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2024-04-12 12:06:48 +0300

    Add project files.

---
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+
+namespace InMemoryApp.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IMemoryCache _memoryCache;
+        public ProductController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+        public IActionResult Index()
+        {
+
+            _memoryCache.Remove("date");
+            
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            if (string.IsNullOrEmpty(_memoryCache.Get<string>("date")))
+            {
+                // get from db
+                var value = DateTime.Now.ToString();
+                _memoryCache.Set<string>("date",value , DateTimeOffset.Now.AddSeconds(20));
+                ViewBag.Date = value;
+            }
+            else
+            {
+                ViewBag.Date = _memoryCache.Get<string>("date");
+            }
+            
+          
+            return View();
+        }
+    }
+}
